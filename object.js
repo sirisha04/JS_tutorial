44fcf660b1e58a8355033b6b7063aeb5b9177b0b
Sirisha Gundu <gsirisha@Virtusa.com>
2018-06-06 16:24:42 +0530

    c

---
+++ object.js
@@ -0,0 +1,11 @@
+
+//person object - propertites declared as name value pairs
+var person={
+    name:"john",height:183
+}
+
+//accessing an object's property in 2 way's
+var x = person.height; 
+var y = person['height'];
+document.write(x);
+document.write(y);
