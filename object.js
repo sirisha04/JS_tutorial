82e94cd410b3c1e2914bf9daf0daecb48dc128a8
Sirisha Gundu <gsirisha@Virtusa.com>
2018-06-06 16:46:14 +0530

    c

--- object.js
+++ object.js
@@ -9,3 +9,26 @@ var x = person.height;
 var y = person['height'];
 document.write(x);
 document.write(y);
+
+//Object Constructor
+function Person(name,height,age){
+    this.name=name;
+    this.height=height;
+    this.age=age;
+    this.changeAge=changeAge;//declaring method inside an object
+}
+
+//change age by using oject's method
+function changeAge(age1){
+    return age1;
+}
+
+
+//creating person object with new keyword
+var jack = new Person("jack",136,23);
+var JackHeight=jack.height;
+document.write(JackHeight);
+
+//accesing object's method
+var agemodified = jack.changeAge(29);
+document.write(agemodified);// returns changed age
